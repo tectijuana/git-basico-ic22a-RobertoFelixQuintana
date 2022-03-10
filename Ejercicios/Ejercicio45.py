7cd84404bdb8fc140f6613eaf87566715a1dd332
RobertoFelixQuintana <78781717+RobertoFelixQuintana@users.noreply.github.com>
2022-03-09 23:27:47 -0800

    Create Ejercicio45.py

---
+++ Ejercicios/Ejercicio45.py
@@ -0,0 +1,12 @@
+numeros= [2,3,4,5,6,7,8,9]
+
+for n in numeros:
+    numero1= pow(n,2)
+    numero2= pow(n,3)
+    numero3= pow(n,4)
+    numero4= pow(n,5)
+
+    print ("El numero",int(n),"a la ^2 es:", numero1)
+    print ("El numero",int(n),"a la ^3 es:", numero2)
+    print ("El numero",int(n),"a la ^4 es:", numero3)
+    print ("El numero",int(n),"a la ^5 es:",numero4)
