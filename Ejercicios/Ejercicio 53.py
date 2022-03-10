03e430279f8c328f873c77afd569acbb76d91b7a
RobertoFelixQuintana <78781717+RobertoFelixQuintana@users.noreply.github.com>
2022-03-10 01:57:34 -0800

    Create Ejercicio 53.py

---
+++ Ejercicios/Ejercicio 53.py
@@ -0,0 +1,10 @@
+print ("Encontrar la forma general de la ecuacion lineal dada las coordenadas (9,7) (5,4) sobre las coordenadas")
+
+x1=8
+x2=5
+y1=7
+y2=4
+
+m=(y2-y1)/(x2-x1)
+
+print("y=",m,"x+2")
