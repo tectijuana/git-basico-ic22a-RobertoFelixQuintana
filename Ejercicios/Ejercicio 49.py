55088fe9b3775206cdbd0f4ad54a08eafa578d77
RobertoFelixQuintana <78781717+RobertoFelixQuintana@users.noreply.github.com>
2022-03-10 00:05:49 -0800

    Create Ejercicio 49.py

---
+++ Ejercicios/Ejercicio 49.py
@@ -0,0 +1,9 @@
+print("Introduce el valor de A")
+a=input()
+print("Introduce el valor de B")
+b=input()
+
+if(abs(int(a)+int(b)) == abs(int(a))+ abs(int(b))):
+    print("Se cumple la condicion")
+else:
+    print("No se cumple la condicion")
