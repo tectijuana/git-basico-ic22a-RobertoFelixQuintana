7a2ce8b71e9d9d538a3aac1048a9ec811c4673d5
RobertoFelixQuintana <78781717+RobertoFelixQuintana@users.noreply.github.com>
2022-03-10 01:37:42 -0800

    Create Ejercicio 52.py

---
+++ Ejercicios/Ejercicio 52.py
@@ -0,0 +1,10 @@
+import math
+print("Evaluar la funcion y=√x cuando x sea de 1 a 10")
+
+x=1
+
+while(x<11):
+    y = math.sqrt(x)
+    print("El valor de y es:",y,"cuando la raiz de",x)
+    x = x + 1
+    
